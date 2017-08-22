da6a95a1eadda2a9bdd74f87f3cc7ba2bc91f5b1
Jean Metz <jean.metz@ravago.com>
2017-08-22 14:55:55 +0200

    fix tunnel script

--- tunnel.sh
+++ tunnel.sh
@@ -16,8 +16,9 @@ usage() {
 	echo "    -p | --port ssh port number                   defaults to 2222"
 	echo "    -bl | --bind-local bind origin port           defaults to 9000"
 	echo "    -bt | --bind-target bind destination port     defaults to 9000"
-    echo "    -v |                                           set verbose mode to true"
-	echo
+	echo "    -n | --off-set integer sets host address accoding to ravago convention (docker number); Can not be used with -h"
+    echo "    -v |                                          set verbose mode to true"
+    echo
 	echo "Example:"
 	echo "    tunnel.sh -h 10.3.11.15 -p 2223"
 }
@@ -30,11 +31,12 @@ fi
 
 USER="webuser"
 DEST_HOST=$DEFAULT_DOCKER_HOST
-SSH_PORT="2222"
+SSH_PORT=2222
 BIND_LOCAL_PORT="9000"
 BIND_TARGET_PORT="9000"
 BIND_TARGET_HOST="localhost"
 VERBOSE=0
+OFF_SET=0
 
 while [[ $# -gt 0 ]]
 do
@@ -63,9 +65,18 @@ do
         -v|--verbose)
             VERBOSE=1
             ;;
+        -n|--off-set)
+            OFF_SET="$2"
+            if ! [[ "$OFF_SET" =~ ^[0-9]+$ ]] ; then
+                echo $OFF_SET
+                error "-n" 5
+            fi
+            SSH_PORT=$(( $SSH_PORT + $OFF_SET ))
+            shift
+            ;;
         *)
             echo "Wrong parameter"
-            exit 3
+            error "$1" 3
             ;;
     esac
     shift
