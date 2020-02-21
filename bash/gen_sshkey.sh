3766970d12e2bd2d1e067a071c8fd3e265e8fdea
Metz <jean.metz@gfk.com>
2020-02-21 16:19:30 +0100

    some changes to ssh scripts

---
+++ bash/gen_sshkey.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+DOMAIN="dummy.com"
+
+function usage {
+    echo "$0 -[hd]"
+    echo "       -h  : shows this message. Ignores any other arguments."
+    echo "       -d  : sets the domain name for you key. (Example: dummy.com)"
+    echo
+}
+
+function err (){
+    local msg=$1
+    echo "[ERROR] - ${msg}"
+    usage
+    exit $2
+}
+
+banner() {
+    msg="| $* |"
+    edge=$(echo "$msg" | sed 's/./~/g')
+    echo "$edge"
+    echo "$msg"
+    echo "$edge"
+}
+
+
+if [ $# -lt 1 ]; then
+    echo 'Missing domain argument'
+    echo
+    usage
+    exit 1
+fi
+
+
+while getopts ":hd:" opt; do
+    case "${opt}" in
+        d)
+            DOMAIN="${OPTARG}"
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        :)
+            err "Option -$OPTARG requires an argument." 2
+            ;;
+        \?)
+            err "Invalid option: -$OPTARG" 1
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+
+
+remaining_arguments=( "$@" )
+if [[ ${#remaining_arguments[*]} -gt 0 ]]; then
+    err "Invalid arguments were given" 3
+fi
+
+
+mkdir -p $HOME/.ssh
+SSH_KEY_NAME="$(whoami)_ssh_$(date +%Y%m%d)@$DOMAIN"
+ssh-keygen -t rsa -b 4096 -C "${SSH_KEY_NAME}" -f "$HOME/.ssh/${SSH_KEY_NAME}.id_rsa"
+
+cat "$HOME/.ssh/${SSH_KEY_NAME}.id_rsa.pub" | pbcopy
+
+echo
+echo
+banner "the PUBLIC key was copied to your clipboard"
+echo
--- bash/tunnel.sh
+++ bash/tunnel.sh
@@ -10,17 +10,20 @@ usage() {
     echo "Usage:"
     echo "   tunnel connect [-n number] [-h host] [-u user] [-p password] [-a app] [-v]"
     echo "   tunnel list"
-    echo "   tunnel spy <port>"
+    echo "   tunnel spyport <port>"
     echo "   tunnel halt <control socket path>"
     echo
     echo "Establish connection options are:"
-    echo "    -u | --user user name (default: webuser)"
-    echo "    -h | --host ip address to the target host (default: \$DEFAULT_DOCKER_HOST)"
-    echo "    -p | --port ssh port number (default: 2222)"
+    echo "    -u | --user user name (default: \$GFK_USER_NAME)"
+    echo "    -h | --host ip address to the target host (default: \$DEV1)"
+    echo "    -p | --port ssh port number (default: 22)"
     echo "    -bl | --bind-local bind origin port (default: 9000)"
     echo "    -bt | --bind-target bind destination port (default: 9000)"
     echo "    -s | --socket control socket for connection sharing (default: '%h%p%r')"
-    echo "    -n | --off-set integer sets host address according to ravago convention (docker number); Can not be used with -h"
+    echo "    -n | --off-set integer sets host address according to ravago convention (docker number)"
+    echo "                   Can not be used with -h"
+    echo "    -k set connect with ssh private key instead of password."
+    echo "           Depends on \$PRIVATE_KEY environment var which should point to the key file"
     echo "    -v   set verbose mode to true"
     echo
     echo "    Examples:"
@@ -31,29 +34,29 @@ usage() {
     echo "    tunnel list"
     echo
     echo "Check connection:"
-    echo "    tunnel spy 60000"
+    echo "    tunnel spyport 22"
     echo
     echo "Close connection options are:"
     echo "    -s | --socket  control socket for connection sharing (default: 'host-sshport-user')"
     echo
     echo "    Example:"
-    echo "      tunnel halt /tmp/10.3.11.62-2223-webuser"
+    echo "      tunnel halt /tmp/10.3.11.62-2223-username"
 
 }
 
-USER="webuser"
-DEST_HOST=$DEFAULT_DOCKER_HOST
-SSH_PORT=2222
+USER=$GFK_USER_NAME
+DEST_HOST=$DEV1
+SSH_PORT=22
 BIND_LOCAL_PORT="9000"
 BIND_TARGET_PORT="9000"
 BIND_TARGET_HOST="localhost"
+USE_PRIVATE_KEY=0
 VERBOSE=0
 OFF_SET=0
 SOCKET="/tmp/$DEST_HOST-$SSH_PORT-$USER"
 
 
 function checkConnection(){
-
     if [ $# -ne 1 ]; then
         usage
         exit 1
@@ -62,7 +65,6 @@ function checkConnection(){
     port=$1
     nc -z localhost $port || echo 'no tunnel open' ; exit 0;
     echo "tunnel active on port $1"; exit 0;    
-
     #https://apple.stackexchange.com/questions/117644/how-can-i-list-my-open-network-ports-with-netstat
 }
 
@@ -71,18 +73,14 @@ function listTunnels(){
 }
 
 function closeConnection(){
-    set -x
     if [[ ! -z "$1" ]]; then
         SOCKET="$1"
     fi
-
     ssh -S $SOCKET -O exit "localhost"
-    set +x
     exit 0;
 }
 
-function establishConnection(){
-
+function parseArgs(){
     while [[ $# -gt 0 ]]
     do
         key="$1"
@@ -119,6 +117,9 @@ function establishConnection(){
                 SSH_PORT=$(( $SSH_PORT + $OFF_SET ))
                 shift
                 ;;
+            -k)
+              USE_PRIVATE_KEY=1
+              ;;
             *)
                 echo "Wrong parameter"
                 error "$1" 3
@@ -126,60 +127,36 @@ function establishConnection(){
         esac
         shift
     done
+}
+
+function connect(){
+    parseArgs "${@}"
+    if [[ $USE_PRIVATE_KEY == 1 ]]; then
+        LOGIN_MODE="-i ${PRIVATE_KEY}"
+    else
+        LOGIN_MODE=""
+    fi
+    ssh $LOGIN_MODE $USER"@"$DEST_HOST -p $SSH_PORT
+}
+
+function createTunnel(){
+    parseArgs "${@}"
 
     if [[ $VERBOSE -eq 1 ]]; then
         echo "Running:"
         set -x
     fi
 
-    # ssh switches meaning:
-    #   -N  Do not execute a remote command.  
-    #         This is useful for just forwarding ports (protocol version 2 only).
-    #   -f Requests ssh to go to background just before command execution.  
-    #         This is useful if ssh is going to ask for passwords or passphrases, 
-    #         but the user wants it in the background. 
-    #   -M  Places the ssh client into “master” mode for connection sharing.  Multiple -M options places ssh
-    #         into “master” mode with confirmation required before slave connections are accepted.  
-    #         Refer to the description of ControlMaster in ssh_config(5) for details.
-    #   -S ctl_path
-    #         Specifies the location of a control socket for connection sharing, or the string “none” to disable
-    #         connection sharing.  Refer to the description of ControlPath and ControlMaster in ssh_config(5) for
-    #         details.
-    #         The ctl_path is a path to a file the master instance of ssh client will create for inter-process
-    #         communication with other ssh client instances that want to share master instance's connection.
-    #         The created file will actually represent a unix domain socket. It can be named and located whatever
-    #         and wherever you want it, e.g. /tmp/session1 (though it is recommended to name it using
-    #         %patterns -- see ControlPath description bellow)
-    #    
-    #         ControlPath
-    #             Specify the path to the control socket used for connection sharing as described in the ControlMaster
-    #             section above or the string ``none'' to disable connection sharing. In the path, `%L' will be
-    #             substituted by the first component of the local host name, `%l' will be substituted by the localhost
-    #             name (including any domain name), `%h' will be substituted by the target host name, `%n' will be
-    #             substituted by the original target host name specified on the command line, `%p' the destination
-    #             port, `%r' by the remote login username, `%u' by the username and `%i' by the numeric user ID (uid)
-    #             of the user running ssh(1), and `%C' by a hash of the concatenation: %l%h%p%r.  It is recommended
-    #             that any ControlPath used for opportunistic connection sharing include at least %h, %p, and %r
-    #             (or alternatively %C) and be placed in a directory that is not writable by other users.
-    #             This ensures that shared connections are uniquely identified.
-    #
-    # -L [bind_address:]port:host:hostport
-    #         Specifies that the given port on the local (client) host is to be forwarded to the given host and
-    #         port on the remote side.  This works by allocating a socket to listen to port on the local side,
-    #         optionally bound to the specified bind_address.  Whenever a connection is made to this port, the
-    #         connection is forwarded over the secure channel, and a connection is made to host port hostport
-    #         from the remote machine.  Port forwardings can also be specified in the configuration file.  IPv6
-    #         addresses can be specified by enclosing the address in square brackets.  Only the superuser can
-    #         forward privileged ports.  By default, the local port is bound in accordance with the GatewayPorts
-    #         setting.  However, an explicit bind_address may be used to bind the connection to a specific
-    #         address.  The bind_address of “localhost” indicates that the listening port be bound for local use
-    #         only, while an empty address or ‘*’ indicates that the port should be available from all
-    #         interfaces.
+    #ssh -i $PRIVATE_KEY -f -N -M -S "$SOCKET" -L "${BIND_LOCAL_PORT}:${BIND_TARGET_HOST}:${BIND_TARGET_PORT}" $USER"@"$DEST_HOST -p $SSH_PORT
+    if [[ $USE_PRIVATE_KEY == 1 ]]; then
+        LOGIN_MODE="-i ${PRIVATE_KEY}"
+    else
+        LOGIN_MODE=""
+    fi
 
-    set -x
-    ssh -f -N -M -S $SOCKET -L "${BIND_LOCAL_PORT}:${BIND_TARGET_HOST}:${BIND_TARGET_PORT}" $USER"@"$DEST_HOST -p $SSH_PORT
-    set +x
-    exit 0;
+    echo "Creating tunnel with socket control: ${SOCKET}"
+    ssh $LOGIN_MODE -f -N -M -S "$SOCKET" -L "${BIND_LOCAL_PORT}:${BIND_TARGET_HOST}:${BIND_TARGET_PORT}" $USER"@"$DEST_HOST -p $SSH_PORT
+    exit 0
 }
 
 
@@ -192,11 +169,14 @@ main() {
     subtask=$1
 
     case $subtask in
-        spy)
+        spyport)
             checkConnection "${@:2}"
             ;;
+        tunnel)
+            createTunnel "${@:2}"
+            ;;
         connect)
-            establishConnection "${@:2}"
+            connect "${@:2}"
             ;;
         halt)
             closeConnection "${@:2}"
